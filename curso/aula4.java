915d9469ad86acee066e40692bcdb76562114096
mantiniDev <pmantinidev@gmail.com>
2022-12-18 18:31:46 -0300

    aula quatro

---
+++ curso/aula4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class aula4{
+    public static void nmain(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        int n1=0, n2=0, res=0;
+
+        System.out.println("Digite um numeral: ");
+        n1 = scan.nextInt();
+    
+    }
+    
+}
