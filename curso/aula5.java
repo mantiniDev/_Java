7c6c667bedb92822abca759abcc3fba0f2dd2e0b
mantiniDev <pmantinidev@gmail.com>
2022-12-20 23:40:47 -0300

    aula cinco

---
+++ curso/aula5.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class aula5 {
+    
+    public static void main(String[] args) {
+
+        Scanner scan=new Scanner(System.in);
+        /*
+        for(int cont= 0; cont <5; cont++){
+            System.out.println("CFB Cursos");
+        }
+        */
+        /*
+        int max = scan.nextInt();
+        int cont = 0;
+        while(cont<max){
+            System.out.println("CFB Cursos");
+            cont++;
+        } */
+        
+        int cont = 0;
+        do{
+            System.out.println("CFB Cursos");
+            cont++;
+        }while(cont <= 0);
+
+
+
+    }   
+
+}
+    
+
