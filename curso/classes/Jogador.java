9295bb749c7992d09e6880170c4d80d0061ef353
mantiniDev <pmantinidev@gmail.com>
2022-12-26 18:32:41 -0300

    aula doze

--- curso/classes/Jogador.java
+++ curso/classes/Jogador.java
@@ -3,13 +3,36 @@ package classes;
 public class Jogador {
     //Metodo construtor não tem retorno
 
+    private final int maxVidas=3;
     private int num=0;
     private int vidas=0;
 
 
     public Jogador(int num){
         this.num=num;
-        this.vidas=3;
+        this.setVidas(3);
         System.out.printf("Jogador numero %d Criado%n", num);
+    }
+
+    public int getVidas(){
+        return this.vidas;
+    }
+
+    private void setVidas(int vidas){
+        if(vidas>maxVidas){
+            this.vidas=maxVidas;
+        }else if(vidas <0){
+            this.vidas=0;
+        }else{
+            this.vidas=vidas;
+        }    
+        
+    }
+
+    public void addVidas(){
+        if(this.vidas < maxVidas){
+            this.vidas++;
+        }      
+        
     }
 }
--- curso/classes/aulaClasses.java
+++ curso/classes/aulaClasses.java
@@ -8,6 +8,11 @@ public class aulaClasses {
         Jogador j1 = new Jogador(++num);
         Jogador j2 = new Jogador(++num);
         
+        j1.addVidas();
+        
+
+
+        System.out.printf("A Vida do jogador 1 é :%d",j1.getVidas());
         //System.out.println(j1.num);
     }
 }
