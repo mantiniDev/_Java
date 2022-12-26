4790ea1062b790c2320097170fb90ea63a790323
mantiniDev <pmantinidev@gmail.com>
2022-12-26 18:00:26 -0300

    aula dez

---
+++ curso/classes/Jogador.java
@@ -0,0 +1,8 @@
+package classes;
+
+public class Jogador {
+    //Metodo construtor não tem retorno
+    public Jogador(int n){
+        System.out.printf("%n Jogador numero %d Criado %n", n);
+    }
+}
